89a763af1098bdbaf2fac394047146ff79428811
OokamiMidori <fabio.miranda.couto@gmail.com>
2025-04-14 15:41:39 -0300

    codigos

---
+++ imc.js
@@ -0,0 +1,41 @@
+
+const calcular = document.getElementById('calcular');
+
+
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== "") {
+        if (altura !== "") {
+            if (peso !== "") {
+                const valorImc = (peso / (altura * altura)).toFixed(2);
+                let classificacao = "";
+                if (valorImc < 18.5) {
+                    classificacao = "abaixo do peso"
+                } else if (valorImc < 25) {
+                    classificacao = "com peso ideal. Parabéns!!"
+                } else if (valorImc < 30) {
+                    classificacao = "levemente acima do peso"
+                } else if (valorImc < 35) {
+                    classificacao = "com obesidade grau 1"
+                } else if (valorImc < 40) {
+                    classificacao = "com obsdidade grau 2"
+                } else {
+                    classificacao = "com obesidade grau 3. Cuidado!!"
+                }
+                resultado.textContent = `${nome} seu IMC é igual á ${valorImc} e você está ${classificacao}`;
+            } else {
+                alert("Peso esta vazio")
+            }
+        } else {
+            alert("Altura esta vazio")
+        }
+    } else {
+        alert("Nome esta vazio")
+    }
+}
+
+calcular.addEventListener('click', imc);
